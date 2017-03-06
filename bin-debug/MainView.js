7a85a993236f004f5401ba8ce2f62119c0079e39
zouyang <7012490@qq.com>
2017-03-06 16:22:27 +0800

    new

---
+++ bin-debug/MainView.js
@@ -0,0 +1,19 @@
+var __reflect = (this && this.__reflect) || function (p, c, t) {
+    p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
+};
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var MainView = (function (_super) {
+    __extends(MainView, _super);
+    function MainView() {
+        var _this = _super.call(this) || this;
+        _this.skinName = "MainSkin";
+        return _this;
+    }
+    return MainView;
+}(eui.Component));
+__reflect(MainView.prototype, "MainView");
+//# sourceMappingURL=MainView.js.map
---
+++ src/MainView.ts
@@ -0,0 +1,13 @@
+class MainView extends eui.Component
+{
+
+    private loginBtn:eui.Button;
+    private regBtn:eui.Button;
+    private nameTxt:eui.TextInput;
+    private pwdTxt:eui.TextInput;
+    public constructor()
+    {
+        super();
+        this.skinName = "MainSkin";
+    }
+}
